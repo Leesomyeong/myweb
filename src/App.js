f58434aedf0ca274ecb55b8823ec31f62e52c5bc
Leesomyeong <69294128+Leesomyeong@users.noreply.github.com>
2020-08-11 23:26:17 +0900

    somyeong

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
+    <div>
     <p> 안녕하십니까~? </p>
+    <Button Primary> hello:) </Button>
+    <Progress percent = {33} indicating/>
+    </div>
   );
 }
 
